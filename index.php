467104493e65b14929cba3d951de112e263d1eb4
imranmuha <imramuha@student.arteveldehogeschool.be>
2017-10-05 15:51:21 +0200

    Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+     
+
+        <?php $string1 = "Hello "?>
+        <?php $string2 = "World"?>
+        <h3><?php echo $string1, $string2?></h3>
+
+        
+    
+</body>
+</html>
